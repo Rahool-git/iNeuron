ecc3a2c32ab1aa6cf8a57100addf4631e49bdd57
rahool <108623674+Rahool-git@users.noreply.github.com>
2024-04-29 16:40:45 +0530

    Add files via upload

---
+++ temperature.js
@@ -0,0 +1,13 @@
+let temp = 60 // input
+
+// to convert temperature in Fahrenheit to Celsius
+temp_celsius = (temp - 32) * 5 /9
+//console.log("Temperature value", temp "in Celsius is", temp_celsius, "C")
+console.log(`Temperature value ${temp} in Celsius is ${temp_celsius} C`)
+
+// to convert temperature in Celsius to Fahrenheit
+temp_fahrenheit = temp * 9/5 +32
+//console.log("Temperature value", temp, "in Fahrenheit is", temp_fahrenheit, "F")
+console.log(`Temperature value ${temp} in Fahrenheit is ${temp_fahrenheit} F`)
+
+
